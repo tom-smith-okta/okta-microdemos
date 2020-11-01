3a9a82a87f923be8e9d5947df697f24669626325
Tom Smith <tom.smith@okta.com>
2020-11-01 18:01:44 -0500

    cleanup

--- public/cvs_health/misc.js
+++ public/cvs_health/misc.js
@@ -1,6 +1,5 @@
 
 function update_ui_panel(auth_state) {
-
 	if (auth_state != "authenticated") {
 		return
 	}
@@ -40,8 +39,6 @@ function evaluate_prefs() {
 		email: claims.email
 	}
 
-	// var url = "https://microdemos.workflows.oktapreview.com/api/flo/3fdc9eabd0ad5d38657ca68f8b6d0eb5/invoke"
-
 	var url = "https://cvsciam20.workflows.oktapreview.com/api/flo/2ea225b63570346ed4406b0f0169fecb/invoke"
 
 	$.post(url, obj)
@@ -50,44 +47,4 @@ function evaluate_prefs() {
 	});
 
 	return
-
-	var id_token = localStorage.getItem('okta-token-storage')
-
-	id_token = JSON.parse(id_token)
-
-	var claims = id_token.id_token.claims
-
-	var user_obj = {
-		user_id: claims.sub
-	}
-
-	var user_prefs_obj = {}
-
-	user_obj.prefs = {}
-
-	var preferences = localStorage.getItem("preferences")
-
-	preferences = JSON.parse(preferences)
-
-	for (pref in preferences) {
-		if ($('#' + pref).is(":checked")) {
-			user_obj.prefs[pref] = true
-		}
-		else {
-			user_obj.prefs[pref] = false
-		}
-	}
-
-	alert(JSON.stringify(user_obj))
-
-	var url = "https://microdemos.workflows.oktapreview.com/api/flo/0240931833c197904f2c1a112b5f90e1/invoke"
-
-	var url = "https://cvsciam20.workflows.oktapreview.com/api/flo/2ea225b63570346ed4406b0f0169fecb/invoke"
-
-https://cvsciam20.workflows.oktapreview.com/api/flo/2ea225b63570346ed4406b0f0169fecb/invoke
-
-	$.post( url, user_obj)
-	.done(function( data ) {
-		$("#ui_panel").html("Thank you for updating your preferences!<br>")
-	});
 }
